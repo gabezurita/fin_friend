947ee3e9c3488b9d357e7cbf2db2c407e3fc0d7a
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 02:04:02 -0400

    Add create new credit card to controller.

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,6 +2,11 @@ class CreditCardsController < ApplicationController
   def new
   end
 
+
   def create
+    @credit_card = CreditCard.new(params[:credit_card])
+
+    @credit_card.save
+    redirect_to @credit_card
   end
 end
