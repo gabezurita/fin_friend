0f75cc3eda4a93046be492a887ace764e4ddc03b
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 00:02:18 -0400

    Add secret page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/secret'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Add a root route if you don't have one...
 	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
