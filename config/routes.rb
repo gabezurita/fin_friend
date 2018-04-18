4dca10e9b311d08c5d4ca410ed00f96203298280
Gabriel Zurita <gabezurita@gmail.com>
2018-04-17 23:35:24 -0400

    Add login and logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
 	# create (post) action for when sign up form is submitted:
 	post 'users' => 'users#create'
 
+	# log in page with form:
+	get '/login'     => 'sessions#new'
+
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'
 end
