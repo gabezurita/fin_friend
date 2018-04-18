47a4c7c834b8e1edbee01829f5d30b56232d7b7b
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 02:01:11 -0400

    fix typo and run migration.

--- db/migrate/20180418055620_create_credit_cards.rb
+++ db/migrate/20180418055620_create_credit_cards.rb
@@ -3,10 +3,11 @@ class CreateCreditCards < ActiveRecord::Migration[5.1]
     create_table :credit_cards do |t|
       t.string :name
       t.text :description
-      t.integercreated :APR
+      t.integer :APR
       t.integer :accrued_interest
       t.integer :balance
       t.integer :limit
+      t.datetime :created
 
       t.timestamps
     end
