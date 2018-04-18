082d87d0ae7cd9b37415c28bc51f35aeac49b3cc
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 00:00:36 -0400

    Add authorize helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  # authroize method redirects user to login page if not logged in:
+  def authorize
+    redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
+  end
+
   def new
     @user = User.new
   end
