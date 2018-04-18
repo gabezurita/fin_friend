b16ce9b62044953bb5319fb7d2536403a808277b
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 19:23:44 -0400

    Add users controller.

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,12 +1,35 @@
 class CreditCardsController < ApplicationController
   def new
+    @user = User.find(params[:user_id])
+    @interests = Interest.all
   end
 
 
   def create
-    @credit_card = CreditCard.new(params[:credit_card])
+    @credit_card = CreditCard.new(credit_card_params)
 
     @credit_card.save
     redirect_to @credit_card
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @interests = Interest.all
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @interests = Interest.all
+    if @User.save
+      redirect_to(@user)
+    else
+      @errors = @interest.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+  def credit_card_params
+    params.require(:credit_card).permit(:user, :text)
   end
 end
