4c5394691251b52dabfd1887f8cf6c766a811373
Gabriel Zurita <gabezurita@gmail.com>
2018-04-19 01:42:02 -0400

    Convert decimal to float for simplicity and speed.

--- db/migrate/20180418055620_create_credit_cards.rb
+++ db/migrate/20180418055620_create_credit_cards.rb
@@ -3,12 +3,12 @@ class CreateCreditCards < ActiveRecord::Migration[5.1]
     create_table :credit_cards do |t|
       t.string :name, default: 'FinFriend'
       t.text :description, default: 'With 3 percent cash back on all purchases, this is the best single credit card there is!'
-      t.decimal :apr, precision: 10, scale: 2, default: 0.35
-      t.decimal :limit, precision: 10, scale: 2, default: 1000.00
-      t.decimal :balance, precision: 10, scale: 2, default: 0
-      t.decimal :accrued_interest, precision: 10, scale: 2, default: 0
-      t.decimal :payments, precision: 10, scale: 2, default: 0
-      t.decimal :charges, precision: 10, scale: 2, default: 0
+      t.float :apr, default: 0.35
+      t.float :limit, default: 1000.00
+      t.float :balance, default: 0
+      t.float :accrued_interest, default: 0
+      t.float :payments, default: 0
+      t.float :charges, default: 0
       t.datetime :created
 
       t.timestamps
