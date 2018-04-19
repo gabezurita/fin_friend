2d5a77588a54fc7f4f552a3526ab9340536ef0d9
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 20:45:54 -0400

    Add model associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: true
+
+	has_many :user_credit_cards
+	has_many :credit_cards, through: :user_credit_cards
+
 end
