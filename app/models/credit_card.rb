308bc881614e0f19c6efa57a30cf164b7ee39297
Gabriel Zurita <gabezurita@gmail.com>
2018-04-19 03:06:47 -0400

    Add all necessary methods to perform interest calculations on credit card.

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,4 +1,49 @@
 class CreditCard < ApplicationRecord
+  attr_accessor :total_accrued_interest
+
   has_many :user_credit_cards
   has_many :users, through: :user_credit_cards
+
+  def created_this_month?
+    created_at.month == Time.now.month && created_at.year == Time.now.year
+  end
+
+  def start_of_month
+    Time.now.utc.start_of_month
+  end
+
+  def end_of_month
+    Time.now.utc.at_end_of_month
+  end
+
+  def current_day_of_month
+    Time.now.day
+  end
+
+  def total_days_in_current_month
+    Time.now.end_of_month.day
+  end
+
+  def daily_interest_rate
+    apr.to_f / 365.to_f
+  end
+
+  def daily_accrued_interest
+    (balance.to_f * daily_interest_rate.to_f) / 100.to_f
+  end
+
+  def total_accrued_interest
+    if created_this_month?
+      daily_accrued_interest * (current_day_of_month - created_at.day)
+    else
+      daily_accrued_interest * current_day_of_month
+    end
+  end
+
+  def apply_interest_to_balance!(current_time = nil)
+    current_time ||= Time.now
+    return unless current_time == end_of_month
+    @balance += total_accrued_interest
+    @balance.save!
+  end
 end
