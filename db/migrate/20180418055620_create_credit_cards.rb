ec6b4dc03206c790440ee17d4f8cdffffd015452
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 22:31:04 -0400

    Update default value of credit card to what assignment requires.

--- db/migrate/20180418055620_create_credit_cards.rb
+++ db/migrate/20180418055620_create_credit_cards.rb
@@ -3,8 +3,8 @@ class CreateCreditCards < ActiveRecord::Migration[5.1]
     create_table :credit_cards do |t|
       t.string :name
       t.text :description
-      t.decimal :apr, default: 19.99
-      t.decimal :limit, default: 9001
+      t.decimal :apr, default: 0.35
+      t.decimal :limit, default: 1000.00
       t.decimal :balance, default: 0
       t.decimal :accrued_interest, default: 0
       t.decimal :payments, default: 0
