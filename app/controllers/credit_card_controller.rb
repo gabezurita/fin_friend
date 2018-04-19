d4614a4d732f3b14a7a555b8f74b1e7b8efc350c
Gabriel Zurita <gabezurita@gmail.com>
2018-04-19 10:10:14 -0400

    Add credit card controller to have 1 to 1 equivelancy between model and controller and to allow users to have multiple cards.

---
+++ app/controllers/credit_card_controller.rb
@@ -0,0 +1,23 @@
+class CreditCardsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @credit_card = CreditCard.new
+  end
+
+  def create
+    @credit_card.save
+
+    if @user.save
+      @credit_card.save
+      redirect_to(@user)
+      UserCreditCard.create!(credit_card_id: @credit_card.id, user_id: @user.id)
+
+      # If user saves in the db successfully:
+      flash[:notice] = "Enjoy your new card!"
+      redirect_to root_path
+    else
+      @errors = @credit_card.errors.full_messages
+      render 'new'
+    end
+  end
+end
