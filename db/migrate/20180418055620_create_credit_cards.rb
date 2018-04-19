331457f26e6ac9245ed595b3bf341ac79af7363a
Gabriel Zurita <gabezurita@gmail.com>
2018-04-18 22:17:11 -0400

    Add seeds and default attributes for a credit card.

--- db/migrate/20180418055620_create_credit_cards.rb
+++ db/migrate/20180418055620_create_credit_cards.rb
@@ -3,12 +3,12 @@ class CreateCreditCards < ActiveRecord::Migration[5.1]
     create_table :credit_cards do |t|
       t.string :name
       t.text :description
-      t.decimal :apr
-      t.decimal :limit
-      t.decimal :balance
-      t.decimal :accrued_interest
-      t.decimal :payments
-      t.decimal :charges
+      t.decimal :apr, default: 19.99
+      t.decimal :limit, default: 9001
+      t.decimal :balance, default: 0
+      t.decimal :accrued_interest, default: 0
+      t.decimal :payments, default: 0
+      t.decimal :charges, default: 0
       t.datetime :created
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -18,12 +18,12 @@ ActiveRecord::Schema.define(version: 20180419005550) do
   create_table "credit_cards", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "APR"
-    t.integer "limit"
-    t.integer "balance"
-    t.integer "accrued_interest"
-    t.integer "payments"
-    t.integer "charges"
+    t.decimal "apr", default: "19.99"
+    t.decimal "limit", default: "9001.0"
+    t.decimal "balance", default: "0.0"
+    t.decimal "accrued_interest", default: "0.0"
+    t.decimal "payments", default: "0.0"
+    t.decimal "charges", default: "0.0"
     t.datetime "created"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
